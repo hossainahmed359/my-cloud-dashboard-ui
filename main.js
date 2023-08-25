874009471351b9ac30633277f8c210951b384e0a
hossainahmed359 <hossainahmed359@gmail.com>
2023-08-25 16:17:04 +0600

    Add right offcanvas open close functionalitys

--- main.js
+++ main.js
@@ -2,9 +2,11 @@ import './assets/styles/main.scss';
 
 import userAddEvent from './script/useAddEvent';
 import useLeftSidebarOffcanvas from './script/useLeftSidebarOffcanvas';
+import userRightOffcanvas from './script/userRightOffcanvas';
 
 const { addEventToMultitpleElements } = userAddEvent();
 const { leftSidebarState, handleLeftSidebarOpen, handleLeftSidebarClose } = useLeftSidebarOffcanvas();
+const { rightOffcanvasState, handleRightOffcanvasOpen, handleRightOffcanvasClose } = userRightOffcanvas();
 
 
 function leftSidebarToggler() {
@@ -26,6 +28,16 @@ const eventItemList = [
         id: 'left-sidebar-toggler-btn',
         event: 'click',
         callback: leftSidebarToggler
+    },
+    {
+        id: 'right-offcanvas-opener-btn',
+        event: 'click',
+        callback: handleRightOffcanvasOpen
+    },
+    {
+        id: 'left-offcanvas-close-btn',
+        event: 'click',
+        callback: handleRightOffcanvasClose
     },
 ]
 
---
+++ script/userRightOffcanvas.js
@@ -0,0 +1,32 @@
+
+export default function userRightOffcanvas() {
+
+    // STATE
+    const rightOffcanvasState = {
+        isOpen: false,
+    }
+
+    // ELEMENT
+    const element = document.getElementById('offcanvas-right');
+
+    // SIDEBAR OPENE
+    function handleRightOffcanvasOpen() {
+        element.classList.add('right-offcanvas-slide-in');
+        element.classList.add('open');
+        setTimeout(function () {
+            element.classList.remove('right-offcanvas-slide-in');
+        }, [300])
+    }
+
+    // SIDEBAR CLOSE
+    function handleRightOffcanvasClose() {
+        element.classList.add('right-offcanvas-slide-out');
+        element.classList.remove('open');
+        setTimeout(function () {
+            element.classList.remove('right-offcanvas-slide-out');
+        }, [300])
+    }
+
+
+    return { rightOffcanvasState, handleRightOffcanvasOpen, handleRightOffcanvasClose }
+}
